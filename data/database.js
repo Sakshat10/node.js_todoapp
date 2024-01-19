f966d36a79ee69bc9acc76fcb4abe3fe9ba3540a
Sakshat Kumar <112819804+Sakshat10@users.noreply.github.com>
2024-01-19 14:01:06 +0530

    Add files via upload

---
+++ data/database.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+
+
+export const connectDB = ()=>{
+    mongoose.connect(process.env.MONGO_URI, {
+    dbName: "backendApi",
+}).then((c) => console.log(`Database connecte with ${c.connection.host}`)).catch((e) => console.log(e));
+
+}
